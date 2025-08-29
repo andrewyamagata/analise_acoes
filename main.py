e68c151006e37d3c5401b64c0f7055e37c0567cd
Andrew Yamagata <andrewyamagata@gmail.com>
2025-08-29 19:22:08 -0300

    input

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
-# from selenium import webdriver
-# from selenium.webdriver.chrome.service import Service
-# from webdriver_manager.chrome import ChromeDriverManager
-
-# driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-
-# driver.get('https://economia.uol.com.br/cotacoes/bolsas/')
-
-# input('')
-
 from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.common.exceptions import WebDriverException
+from time import sleep
 
-try:
-    options = webdriver.ChromeOptions()
-    options.add_experimental_option("detach", True)  # Mantém navegador aberto
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-    driver.get("https://www.google.com")
-    input("Pressione Enter para sair...")
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-except WebDriverException as e:
-    print("❌ Erro do WebDriver:", e)
-except Exception as e:
-    print("❌ Erro inesperado:", e)
+driver.get('https://economia.uol.com.br/cotacoes/bolsas/')
+
+input_busca = driver.find_element(By.ID, 'filled-normal')
+input_busca.send_keys('PETR3.SA')
+sleep(3)
+
+input_busca.send_keys(Keys.ENTER)
+sleep(3)
+
+span_val = driver.find_element(By.XPATH, '//span[@class="chart-info-val ng-binding"]')
+cotacao_valor = span_val.text
+print(f'Valor da cotação PETR3: {cotacao_valor}')
+
+input('')
